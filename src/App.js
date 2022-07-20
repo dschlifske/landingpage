7bdcc284a2df7e9fbbb13b7cf71af7baf723a032
dschlifske <dschlifske@gmail.com>
2022-07-19 22:55:32 -0500

    added ui components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { HeroLayout3 } from './ui-components';
+import { Features2x2 } from './ui-components';
 import logo from './ancala health logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import "@aws-amplify/ui-react/styles.css";
+import {AmplifyProvider} from "@aws-amplify/ui-react";
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
 
 function App() {
   return (
+    <AmplifyProvider>
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="Ancala Health"  />
+        {/*<img src={logo} alt="Ancala Health"  />*/}
+        <h1>Ancala Health</h1>
+        <HeroLayout3 />
+        <p />
+        <Features2x2 />
         <p>
           Click <code>Login</code> to get started.
         </p>
+        
         <a
           className="App-link"
           href="https://master.d9tjbwapov1ny.amplifyapp.com/"
@@ -18,6 +32,7 @@ function App() {
         </a>
       </header>
     </div>
+    </AmplifyProvider>
   );
 }
 
